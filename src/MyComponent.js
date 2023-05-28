dc81fc669f22e59cb6a9363ff7070eb0d2765547
wxxd-fxrest <fxrest704@gmail.com>
2023-05-28 14:23:47 +0900

    github 연동

---
+++ src/MyComponent.js
@@ -0,0 +1,15 @@
+
+const MyComponent = ({ name, children }) => {
+
+    MyComponent.defaultProps = {
+        name: "기본 이름"
+    }
+    return(
+        <div>
+            안녕하세요, 제 이름은 {name}입니다. <br/> 
+            children 값은 {children}입니다. 
+        </div>
+    )
+}
+
+export default MyComponent ; 
